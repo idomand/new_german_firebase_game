6e8384375faa5b3d5e46aae5e2ab3adbb4dd6690
Ido Mandelman <idomand@gmail.com>
2022-11-06 16:29:33 +0100

    2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from "./logo.svg";
+import React from "react";
+
 import "./App.css";
 import Home from "./Components/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <Home />
-      </header>
+      <Home />
     </div>
   );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,6 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <div>Home</div>
       <p>this is the new app</p>
     </>
   );
